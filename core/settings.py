935850b75be92bc8cf33f39b2489078f3e9a664f
Luan Glad <luan.glad@outlook.com>
2026-01-16 22:02:21 -0300

    feat: initial project setup with apps and app configs

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'clientes.apps.ClientesConfig',
+    'ordens.apps.OrdensConfig',
+    'usuarios.apps.UsuariosConfig',
 ]
 
 MIDDLEWARE = [
